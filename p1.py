35cb89319013af95833f4d07d18d710a7ea845c6
helisha2005 <helishaprajapati2005@gmail.com>
2025-08-04 09:02:45 -0700

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,23 @@
+
+
+
+
+a=int(input('enter a:'))
+b=int(input('enter b:'))
+cal=input('enter the method:')
+mul=a*b
+div = a/b
+sub = a-b
+add = a+b
+if cal=='mul':
+    print("ans of mul",mul)
+elif cal == 'div':
+    print("ans of div",div)
+elif cal == 'sub':
+    print("ans of sub",sub)
+elif cal == 'add':
+    print("ans of add",add)
+else:
+    print("fail")
+   
+
